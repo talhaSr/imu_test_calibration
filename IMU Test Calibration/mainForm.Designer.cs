3504444ed36e2b46cecda46abb5108595840d08b
Talha Sarı <talhasari4343@outlook.com.tr>
2021-01-27 17:16:05 +0300

    form icon added

--- IMU Test Calibration/mainForm.Designer.cs
+++ IMU Test Calibration/mainForm.Designer.cs
@@ -29,13 +29,18 @@ namespace IMU_Test_Calibration
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(f_main));
             this.SuspendLayout();
             // 
             // f_main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "f_main";
             this.Text = "IMU Test and Calibration";
             this.ResumeLayout(false);
